33cda8dc69f76a61577b72df9256420670b18058
Colton Watkins <watkincw@gmail.com>
2022-04-05 14:31:22 -0500

    created carousel and navbar; implesments placeholder values for now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from "./logo.svg";
+// Bootstrap
+import { Carousel, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+// Personal Style
 import "./App.css";
+// temporary background photos
+import mountains1 from './images/mountains1.jpg';
+import mountains2 from './images/mountains2.jpg';
+import mountains3 from './images/mountains3.jpg';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+
+            <Navbar bg='light' expand='lg'>
+                <Container>
+                    <Navbar.Brand href='#home'>MARS - Feb. 2022 Cohort Group Portfolio</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">A Third Action</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">After Divider</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            <Carousel>
+                <Carousel.Item>
+                    <img className="d-block w-100" src={mountains1} alt="FirstCarousel" />
+                    <Carousel.Caption>
+                        <h3 style={{color: 'black'}}>First slide label</h3>
+                        <p style={{color: 'black'}}>
+                            Nulla vitae elit libero, a pharetra augue mollis
+                            interdum.
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={mountains2}
+                        alt="Second slide"
+                    />
+                    <Carousel.Caption>
+                        <h3 style={{color: 'black'}}>Second slide label</h3>
+                        <p style={{color: 'black'}}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit.
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={mountains3}
+                        alt="Third slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>
+                            Praesent commodo cursus magna, vel scelerisque nisl
+                            consectetur.
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+            
         </div>
     );
 }
